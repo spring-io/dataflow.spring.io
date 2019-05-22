bd42c253a1924b9b6199dcb267ceba40349ccafe
Damien Vitrac <dvitrac@pivotal.io>
2019-05-22 10:06:18 -0500

    Up cookies modal

--- src/components/common/layout/Layout.js
+++ src/components/common/layout/Layout.js
@@ -1,6 +1,5 @@
 import '../../../styles/app.scss'
 
-import Helmet from 'react-helmet'
 import PropTypes from 'prop-types'
 import React from 'react'
 
@@ -9,12 +8,6 @@ import Header from './Header'
 
 const Layout = ({ children, className }) => (
   <>
-    <Helmet>
-      <script
-        src='https://consent.trustarc.com/notice?domain=pivotal.com&c=teconsent&js=nj&text=true&pcookie&gtm=1'
-        async='async'
-      />
-    </Helmet>
     <div className={className}>
       <Header />
       <main>{children}</main>
