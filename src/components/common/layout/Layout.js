d7cc504d7047a59555dd39249abed869bb8721de
onobc <chris.bono@gmail.com>
2024-04-30 17:24:10 -0500

    Revert "feat: SP1 banner"
    This reverts commit fc070d949adcae5d45132de7faa0b0008967171e.

--- src/components/common/layout/Layout.js
+++ src/components/common/layout/Layout.js
@@ -9,15 +9,6 @@ import Header from './Header'
 const Layout = ({ children, version, className }) => (
   <>
     <div className={className}>
-      <div className='springone-banner'>
-        <div>
-          SpringOne Call for papers is open—
-          <a href='https://springone.io' target='_blank'>
-            Submit your talk
-          </a>{' '}
-          by May 3!
-        </div>
-      </div>
       <Header version={version} />
       <main>{children}</main>
       <Footer />
